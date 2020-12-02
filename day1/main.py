08a8391ad46ffa49f3fa1cfaa9de1afe92ac4373
John Chiu <john.chiu@workday.com>
2020-12-01 23:30:21 -0800

    day 1 - use reduce.

--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,6 @@
 from itertools import combinations
+from operator import mul
+from functools import reduce
 
 lines = open('input','r').read().splitlines()
 vals = list(map(int, lines))
@@ -8,7 +10,7 @@ combs = list(combinations(vals, 2))
 sums = list(map(sum, combs))
 for index, value in enumerate(sums):
   if value == 2020:
-    print('Combination: {}.  Product: {}'.format(combs[index], combs[index][0] * combs[index][1]))
+    print('Combination: {}.  Product: {}'.format(combs[index], reduce(mul, combs[index])))
 
 
 # 3 combinations, sum, find 2020
@@ -16,5 +18,4 @@ combs = list(combinations(vals, 3))
 sums = list(map(sum, combs))
 for index, value in enumerate(sums):
   if value == 2020:
-    print('Combination: {}.  Product: {}'.format(combs[index], combs[index][0] * combs[index][1] * combs[index][2]))
-
+    print('Combination: {}.  Product: {}'.format(combs[index], reduce(mul, combs[index])))
