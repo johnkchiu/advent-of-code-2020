9c43b51f8ebca353fa2ab71b39004cac8d5a0e77
John Chiu <john.chiu@workday.com>
2020-12-05 00:41:46 -0800

    day 4 part 1 cleanup.

--- day4/main.py
+++ day4/main.py
@@ -1,31 +1,19 @@
 
+from os import replace
+
+
 valid = 0
 
+# part 1
 with open('input', 'r') as file:
-  has_fields, fields = {}, []
+  # read whole file and split
+  for line in file.read().split('\n\n'):
+    # remove trailing \n, join lines, split by ' ', and then split by ':'
+    fields = [x.split(':') for x in line.strip().replace('\n', ' ').split(' ')]
 
-  for line in file:
-    if line == '\n':
-      # # init map
-      # has_fields = { 'byr':False, 'iyr':False, 'eyr':False, 'hgt':False, 'hcl':False, 'ecl':False, 'pid':False, 'cid':False}
-      # # update fields provided
-      # for field in fields:
-      #   has_fields[field[0]] = True
-      # # check all provide fields
-      # if all((val == True or key == 'cid') for key, val in has_fields.items()):
-      #   # print(fields)
-      #   valid += 1
-      # else:
-      #
+    if len(fields) == 8: valid += 1
+    if len(fields) == 7 and all(field[0] != 'cid' for field in fields): valid += 1
 
-      if len(fields) == 8: valid += 1
-      if len(fields) == 7 and all(field[0] != 'cid' for field in fields): valid += 1
-
-      print(f"Fields[{len(fields)}]: {fields}")
-
-      fields = []
-    else:
-      fields = fields + [x.split(':') for x in line.strip().split(' ')]
+    print(f"Fields[{len(fields)}]: {fields}")
 
 print(valid)
-
